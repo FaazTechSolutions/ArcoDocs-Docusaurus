fb67e6c37750ae137213adbbb72fb72ecbd28f67
m.afrith <m.afrith@faaztechsolutions.com>
2025-06-27 20:19:28 +0530

    Title changes

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Arco Documentation',
+  tagline: 'Documents By Docusaurus',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
